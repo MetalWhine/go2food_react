cb313cbd74d7ce300a9caa4939d304185675cdb2
Bumskee <fadhlanrazan@gmail.com>
2024-04-24 01:47:13 +1000

    added simple app bar

---
+++ src/components/Appbar.js
@@ -0,0 +1,31 @@
+import React from "react";
+import CommentOutlinedIcon from '@mui/icons-material/CommentOutlined';
+import NotificationsOutlinedIcon from '@mui/icons-material/NotificationsOutlined';
+import SettingsOutlinedIcon from '@mui/icons-material/SettingsOutlined';
+import "./styles/Appbar.css"
+
+function Appbar() {
+    return (
+        // main container
+        <div className="bg-white flex justify-between items-center h-16 mx-auto px-4">
+            <h1 className="w-full text-3xl font-bold">Go2Food</h1>
+
+            {/* left most element (buttons and profile picture) main container */}
+            <div className="flex">
+
+                {/* buttons container */}
+                <div className="flex items-center px-4">
+                        <div className="p-2"><CommentOutlinedIcon /></div>
+                        <div className="p-2"><NotificationsOutlinedIcon /></div>
+                        <div className="p-2"><SettingsOutlinedIcon /></div>
+                </div>
+
+                <div >
+                    <img class="max-w-12 max-h-12 rounded-md" src={"images/profile_picture_default.jpg"} alt="Rounded avatar"></img>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Appbar;
--- src/index.js
+++ src/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Appbar from './components/Appbar'
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <Appbar />
     <App />
   </React.StrictMode>
 );
