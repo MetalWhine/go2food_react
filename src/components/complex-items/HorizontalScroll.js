eb60ba680d89e85cf9b449751dfa6f1af9f9e850
Bumskee <fadhlanrazan@gmail.com>
2024-05-06 15:53:12 +1000

    removed mouse drag behaviour on horizontal scroll bar because of unforeseen behaviour conflict with the buttons, changed the way of default preventing on the onwheel listeners of the horizontal scroll elements

--- src/components/complex-items/HorizontalScroll.js
+++ src/components/complex-items/HorizontalScroll.js
@@ -2,39 +2,38 @@ import React, { useRef, useState } from 'react';
 
 const HorizontalScroll = ({ children, className, scrollEndFunc=() => {} }) => {
   const scrollContainerRef = useRef(null);
-  const [isDragging, setIsDragging] = useState(false);
-  const [startX, setStartX] = useState(0);
+  // const [isDragging, setIsDragging] = useState(false);
+  // const [startX, setStartX] = useState(0);
 
-  const handleMouseDown = (event) => {
-    setIsDragging(true);
-    setStartX(event.clientX);
-  };
+  // const handleMouseDown = (event) => {
+  //   setIsDragging(true);
+  //   setStartX(event.clientX);
+  // };
 
-  const handleMouseUp = () => {
-    setIsDragging(false);
-    return;
-  };
+  // const handleMouseUp = () => {
+  //   setIsDragging(false);
+  //   return;
+  // };
 
-  const handleMouseMove = (event) => {
-    if (!isDragging) return;
-    const currentScrollLeft = scrollContainerRef.current.scrollLeft
-    const deltaX = event.clientX - startX;
-    scrollContainerRef.current.scrollLeft -= deltaX;
-    setStartX(event.clientX); // Update startX for next drag movement
-    const isAtEnd = scrollContainerRef.current.scrollLeft + scrollContainerRef.current.clientWidth >= scrollContainerRef.current.scrollWidth;
-    if (isAtEnd) {
-      if (currentScrollLeft !== scrollContainerRef.current.scrollLeft)
-      {
-        scrollEndFunc()
-        console.log("end")
-      }
-    }
-  };
+  // const handleMouseMove = (event) => {
+  //   if (!isDragging) return;
+  //   const currentScrollLeft = scrollContainerRef.current.scrollLeft
+  //   const deltaX = event.clientX - startX;
+  //   scrollContainerRef.current.scrollLeft -= deltaX;
+  //   setStartX(event.clientX); // Update startX for next drag movement
+  //   const isAtEnd = scrollContainerRef.current.scrollLeft + scrollContainerRef.current.clientWidth >= scrollContainerRef.current.scrollWidth;
+  //   if (isAtEnd) {
+  //     if (currentScrollLeft !== scrollContainerRef.current.scrollLeft)
+  //     {
+  //       scrollEndFunc()
+  //       console.log("end")
+  //     }
+  //   }
+  // };
 
   const handleWheel = (event) => {
-    if (event.deltaY === 0) return;
     const currentScrollLeft = scrollContainerRef.current.scrollLeft
-    scrollContainerRef.current.scrollBy(event.deltaY/8, 0);
+    scrollContainerRef.current.scrollBy(event.deltaY/2, 0);
     const isAtEnd = scrollContainerRef.current.scrollLeft + scrollContainerRef.current.clientWidth >= scrollContainerRef.current.scrollWidth;
     if (isAtEnd) {
       if (currentScrollLeft !== scrollContainerRef.current.scrollLeft)
@@ -56,15 +55,16 @@ const HorizontalScroll = ({ children, className, scrollEndFunc=() => {} }) => {
   return (
     <div
       ref={scrollContainerRef}
-      onMouseDown={handleMouseDown}
-      onMouseLeave={handleMouseUp}
-      onMouseUp={handleMouseUp}
-      onMouseMove={handleMouseMove}
+      // onMouseDown={handleMouseDown}
+      // onMouseLeave={handleMouseUp}
+      // onMouseUp={handleMouseUp}
+      // onMouseMove={handleMouseMove}
       onWheel={handleWheel}
       onTouchMove={handleTouchMove}
       style={{
         overflowX: 'scroll',
         display: 'flex',
+        overscrollBehavior: 'contain',
       }}
       className={className}
     >
